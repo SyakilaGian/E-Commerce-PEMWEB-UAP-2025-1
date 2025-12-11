8d924a45faaff77f1c595b8b4a5bdf700d414d20
Your Name <you@example.com>
2025-12-12 00:05:25 +0700

    fitur karin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(_DIR_))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: _DIR_ . '/../routes/web.php',
-        commands: _DIR_ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\IsAdmin::class, // Kustomisasi Anda
-        ]);
+    ->withMiddleware(function (Middleware $middleware) {
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
